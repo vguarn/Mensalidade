852d1473addde57cb1284246d04346f87101086d
Usuario <Usuario@CPD02-M14>
2024-03-26 11:40:20 -0300

    Adicionar arquivos de projeto.

---
+++ Mensalidade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensalidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor da anuidade do curso: ");
+             double anuidade = double.Parse(Console.ReadLine());
+             double mensalidade = anuidade / 12;
+            Console.WriteLine("Digite o valor da matrícula: ");
+             double matricula = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do material do curso: ");
+             double material = double.Parse(Console.ReadLine());
+
+            int cont = 2;
+
+            double mensalidade1 = mensalidade + material + matricula;
+            Console.WriteLine("Mensalidade 1, valor: " +mensalidade1);
+
+            while (cont >1 && cont <=12) 
+            { 
+                Console.WriteLine("Mensalidade " + cont + "valor: " +mensalidade);
+                cont++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
